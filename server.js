f60375d7bc62c63b66a4de922d6d356cc1e6b8bc
shahrukh <shahrukh@aalekhai.com>
2020-05-09 14:21:41 +0530

    Bug fixes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
-app.use("/api/events", events);
+app.use("/api/events",passport.authenticate('jwt', {session: false}), events);
 
 
 app.post('/notifications/subscribe', (req, res) => {
